6fe3e2e82eebd22112eb91f52affdecce786b5c3
Giuseppe Gabbia <beppegabbia@libero.it>
2019-04-23 21:23:47 +0200

    Almost finished

--- Mazzei/_main_.py
+++ Mazzei/_main_.py
@@ -1,8 +1,7 @@
 from pathlib import Path
 from nltk import CFG
-from nltk.tree import Tree, Nonterminal
 import numpy
-import Mazzei.Node as Node
+from nltk import Tree, Nonterminal
 
 
 def main():
@@ -14,13 +13,18 @@ def main():
                  "Noi siamo illuminati",                #Illuminati noi siamo
                  "Tu hai molto da apprendere ancora",   #Molto da apprendere ancora tu hai
                  "Skywalker corre veloce",              #Veloce Skywalker corre
-                 "Skywalker sarà tuo apprendista",      #Tuo apprendista Skywalker sarà
-                 "Frase volutamente non supportata dalla grammatica"]
+                 "Skywalker sarà tuo apprendista"]      #Tuo apprendista Skywalker sarà
+
 
     with open(grammar_file, encoding='utf-8') as file:
         grammar = CFG.fromstring(file.read())
 
-    cky(sentences[5].split(), grammar)
+    translation_rules = [Nonterminal('ADJP'), Nonterminal('ADJ'),
+                         Nonterminal('VBN'), Nonterminal('ADVP')]
+
+    syntactic_tree = cky(sentences[0].split(), grammar)
+    syntactic_tree.draw()
+    yoda_translation(syntactic_tree, translation_rules)
 
 
 def cky(words: list, grammar: CFG):
@@ -29,25 +33,16 @@ def cky(words: list, grammar: CFG):
 
     for j in range(1, table_dimension):
         table[j - 1, j] = list()
-        '''table[j - 1, j].append(Node.Node(find_head_lr(words[j - 1], grammar),
-                                         Node.Node(words[j - 1], None, None), None))'''
-
         table[j - 1, j].append(Tree(find_head_lr(words[j - 1], grammar), [words[j - 1]]))
         for i in reversed(range(0, j - 1)):
             table[i, j] = list()
             for k in range(i + 1, j):
                 if table[i, k] is not None and table[k, j] is not None:
-                    '''table[i, j].append(Node.Node(find_head_gr(list(table[i, k])[0], list(table[k, j])[0], grammar),
-                                                 list(table[i, k])[0], list(table[k, j])[0]))'''
-
                     table[i, j].append(Tree(find_head_gr(list(table[i, k])[0], list(table[k, j])[0], grammar),
-                                            [table[i, k][0], table[k, j][0]]))
+                                            [list(table[i, k])[0], list(table[k, j])[0]]))
 
-    return table[0, table_dimension - 1][0].draw() if table[0, table_dimension - 1][0] != 0 \
-        else Tree("Grammar error", []).draw()
-    '''cky_res = [x for x in list(table[0, table_dimension - 1]) if str(x.data) == "S"]
-    if cky_res:
-        return cky_res[0]'''
+    return table[0, table_dimension - 1][0] if table[0, table_dimension - 1][0] != 0 \
+        else Exception("Sentence not supported by written grammar!")
 
 
 def find_head_lr(word: str, grammar: CFG):
@@ -64,12 +59,17 @@ def find_head_gr(first: Nonterminal, second: Nonterminal, grammar: CFG):
     if grammar_rules:
         return grammar_rules[0].lhs()
 
-'''def find_head_gr(first: Node, second: Node, grammar: CFG):
-    grammar_rules = list((prod for prod in grammar.productions()
-                          if len(prod.rhs()) == 2 and first.data == prod.rhs()[0] and second.data == prod.rhs()[1]))
 
-    if grammar_rules:
-        return grammar_rules[0].lhs()'''
+def yoda_translation(root: Tree, translation_rules: list):
+    to_be_moved = list(filter(lambda i: isinstance(root[i], Tree) and root[i].label() in translation_rules
+                                     and 'put_back' not in locals(), root.treepositions()))[0] if len(root) != 0 else []
+
+    if len(to_be_moved) != 0:
+        prefix = root.__getitem__(to_be_moved)
+        root.__setitem__(to_be_moved, Tree("ε", ["ε"]))
+        root = Tree(Nonterminal('Yoda Translation'), [prefix, root[0], root[1]])
+
+    root.draw()
 
 
 main()
